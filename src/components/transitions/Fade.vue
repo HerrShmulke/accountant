9f11ec4dd805250984b100ce6d7868994f03f140
Alexey Korchuganov <shmulter@mail.ru>
2022-04-04 00:37:04 +0700

    feat: add fade transition

---
+++ src/components/transitions/Fade.vue
@@ -0,0 +1,19 @@
+<template>
+  <Transition name="fade">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="scss">
+.fade {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.15s linear;
+  }
+
+  &-leave-to,
+  &-enter-from {
+    opacity: 0;
+  }
+}
+</style>
