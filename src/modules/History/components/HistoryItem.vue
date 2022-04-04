55f6bd85f98ce65c007338fb9ac79836d5bc6b7d
Alexey Korchuganov <shmulter@mail.ru>
2022-04-05 02:47:57 +0700

    feat: add history item component

---
+++ src/modules/History/components/HistoryItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <ImageViewer v-if="image" v-model="viewerIsOpen" :src="image" :name="name" />
+
+  <VCard class="history-item" v-bind="$attrs">
+    <div class="history-item__content">
+      <h2 class="text-body-1 mb-1">{{ name }}</h2>
+      <div class="text-h2 font-weight-medium text-color-primary-600">{{ price }}₽</div>
+    </div>
+
+    <div class="history-item__actions">
+      <VButton v-if="image" icon aria-label="Открыть изображение" @click="openViewer">
+        <VIcon width="16" height="16" name="image" />
+      </VButton>
+
+      <VButton icon aria-label="Удалить" @click="remove">
+        <VIcon width="16" height="16" name="cross" />
+      </VButton>
+    </div>
+  </VCard>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import VIcon from '@/components/VIcon.vue';
+import VButton from '@/components/VButton.vue';
+import ImageViewer from '@/components/ImageViewer.vue';
+
+let viewerIsOpen = $ref(false);
+
+const props = defineProps({
+  name: { type: String, required: true },
+  price: { type: [String, Number], required: true },
+  image: { type: String, default: null },
+});
+
+const emit = defineEmits(['remove']);
+
+function openViewer() {
+  viewerIsOpen = true;
+}
+
+function remove() {
+  emit('remove');
+}
+</script>
+
+<style lang="scss">
+.history-item {
+  display: flex;
+  justify-content: space-between;
+
+  &__actions {
+    display: grid;
+    justify-content: flex-end;
+    flex-grow: 1;
+    grid-template-columns: repeat(auto-fit, 26px);
+    column-gap: 8px;
+  }
+}
+</style>
