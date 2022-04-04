5bb218e680500f30173714827e024b95ca816263
Alexey Korchuganov <shmulter@mail.ru>
2022-04-05 02:47:24 +0700

    feat: add image viewer component

---
+++ src/components/ImageViewer.vue
@@ -0,0 +1,43 @@
+<template>
+  <VOverlay
+    v-model="model"
+    class="d-flex align-center justify-center image-viewer"
+    content-classes="d-flex align-center flex-column"
+  >
+    <div class="mb-4 font-weight-medium text-h4 text-color-white">{{ name }}</div>
+    <img class="image-viewer__image" :src="src" alt="" />
+  </VOverlay>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed } from 'vue';
+import VOverlay from '@/components/VOverlay.vue';
+
+const props = defineProps({
+  src: { type: String, required: true },
+  name: { type: String, required: true },
+  modelValue: { type: Boolean, default: false },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const model = computed({
+  get() {
+    return props.modelValue;
+  },
+
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
+</script>
+
+<style lang="scss">
+.image-viewer {
+  &__image {
+    width: calc(100% - map-get($--spacers, 8));
+    border-radius: 20px;
+    object-fit: cover;
+  }
+}
+</style>
