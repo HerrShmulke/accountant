d2ffa99826959a53bcde003479986042f58e7d53
Alexey Korchuganov <shmulter@mail.ru>
2022-04-05 02:46:21 +0700

    fix: icon view box

--- src/components/VIcon.vue
+++ src/components/VIcon.vue
@@ -1,5 +1,13 @@
 <template>
-  <svg width="24" height="24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" v-html="icon"></svg>
+  <svg
+    class="v-icon"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    v-html="icon"
+  ></svg>
 </template>
 
 <script setup>
