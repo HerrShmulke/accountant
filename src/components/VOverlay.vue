7e9c77cdf2d65da3eb394167e7cc431ba59683fa
Alexey Korchuganov <shmulter@mail.ru>
2022-04-05 02:47:14 +0700

    feat: add base overlay component

---
+++ src/components/VOverlay.vue
@@ -0,0 +1,71 @@
+<template>
+  <Teleport to="body">
+    <FocusTrap :active="modelValue" :initial-focus="() => $refs.overlay.focus()">
+      <div>
+        <div
+          v-if="modelValue"
+          ref="overlay"
+          class="v-overlay"
+          role="button"
+          tabindex="0"
+          aria-label="Закрыть модальное окно"
+          @click="close"
+          v-bind="$attrs"
+        >
+          <div @click.stop="" :class="contentClasses">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </FocusTrap>
+  </Teleport>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<script setup>
+import { defineProps, defineEmits, watch, useAttrs } from 'vue';
+import { hideScroll, showScroll } from '@/utils/scroll';
+import { FocusTrap } from 'focus-trap-vue';
+
+const props = defineProps({
+  modelValue: { type: Boolean, default: false },
+  contentClasses: { type: [String, Object, Array], default: '' },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    if (value) {
+      hideScroll();
+      return;
+    }
+
+    showScroll();
+  },
+  {
+    immediate: true,
+  }
+);
+
+function close() {
+  emit('update:modelValue', false);
+}
+</script>
+
+<style lang="scss">
+.v-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(map-get($--theme, 'grey', '900'), 0.5);
+}
+</style>
