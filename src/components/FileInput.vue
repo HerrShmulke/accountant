e74b804d0af6e7d8c1f0e803b006beccf36e3d1d
Alexey Korchuganov <shmulter@mail.ru>
2022-04-07 02:07:48 +0700

    refactor: file input minor changes

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -1,36 +1,39 @@
 <template>
-  <label class="file-input">
-    <input
-      class="file-input__field"
-      type="file"
-      accept="image/*"
-      capture
-      @change="onChange"
-    >
-    <img
-      v-if="currentFileUrl"
-      class="file-input__image"
-      :src="currentFileUrl"
-      alt=""
-    >
+  <label class="file-input" tabindex="0" @keyup.space="openFileInput">
+    <input ref="input" class="file-input__field" type="file" accept="image/*" capture @change="onChange" />
+    <img v-if="currentFileUrl" class="file-input__image" :src="currentFileUrl" alt="" />
     <span v-else>Добавить фото</span>
   </label>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, defineEmits, watch } from 'vue';
 const reader = new FileReader();
 
 let currentFileUrl = ref('');
+
+const emit = defineEmits(['change']);
+/** @type {import('vue').Ref<HTMLInputElement>} */
+const input = ref(null);
+
+watch(
+  () => currentFileUrl.value,
+  (value) => {
+    emit('change', value);
+  }
+);
 
 reader.addEventListener('loadend', () => {
   currentFileUrl.value = reader.result;
 });
 
 function onChange(event) {
-  console.dir(event.target.files[0]);
   reader.readAsDataURL(event.target.files[0]);
   event.target.value = '';
+}
+
+function openFileInput() {
+  input.value.click();
 }
 </script>
 
