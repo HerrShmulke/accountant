e21b3641b5c9eaea61be87bdb5c506653a701432
Alexey Korchuganov <shmulter@mail.ru>
2022-04-07 02:05:54 +0700

    feat: update new eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:vue/base', 'plugin:vue/vue3-essential'],
   rules: {
     'vue/max-attributes-per-line': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-self-closing': 0,
   },
 };
