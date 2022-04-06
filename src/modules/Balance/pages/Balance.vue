20e3a00cb0ea337645fab47f14137249c2dd8a75
Alexey Korchuganov <shmulter@mail.ru>
2022-04-07 03:14:28 +0700

    refactor: balance

--- src/modules/Balance/pages/Balance.vue
+++ src/modules/Balance/pages/Balance.vue
@@ -1,79 +1,53 @@
 <template>
-  <Teleport to="#title">
-    Баланс
-  </Teleport>
+  <Teleport to="#title">Баланс</Teleport>
 
   <VCardList>
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Потрачено
-      </h2>
-      <div
-        class="text-h2 font-weight-medium"
-        :style="{ color: spentColor }"
-      >
-        {{ spent }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Потрачено</h2>
+      <div class="text-h2 font-weight-medium" :style="{ color: spentColor }">{{ spent }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Рекомендуемая трата в день
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ recommendedSpending }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Рекомендуемая трата в день</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ recommendedSpending }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Сумма
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ amount }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Сумма</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ amount }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        В копилке
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ moneybox }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">В копилке</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ moneybox }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Всего потрачено за половину
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ spentInHalf }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Всего потрачено за половину</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ spentInHalf }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Сумма на половину месяца
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ amountForHalfMonth }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Сумма на половину месяца</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ amountForHalfMonth }}₽</div>
     </VCard>
 
     <VCard>
-      <h2 class="text-body-1 mb-1">
-        Сумма запланированных трат
-      </h2>
-      <div class="text-h2 text-color-primary-600 font-weight-medium">
-        {{ plannedExpenditures }}₽
-      </div>
+      <h2 class="text-body-1 mb-1">Сумма запланированных трат</h2>
+      <div class="text-h2 text-color-primary-600 font-weight-medium">{{ plannedExpenditures }}₽</div>
     </VCard>
   </VCardList>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue';
+
+// Services
+import { getBalance } from '@/services/balance';
+
+getBalance().then((balance) => {
+  amount.value = balance;
+});
 
 const spent = ref(0);
 const recommendedSpending = ref(1120);
