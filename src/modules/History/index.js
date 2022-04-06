0cb8cd215b680279a0ffecee5cd097af1ac29cc8
Alexey Korchuganov <shmulter@mail.ru>
2022-04-07 03:13:43 +0700

    feat: add hook for history

--- src/modules/History/index.js
+++ src/modules/History/index.js
@@ -1,4 +1,16 @@
+import { router as $router } from '@/plugins/router';
+
+// Services
+import { setHook } from '@/services/hook';
+
+// Constants
+import { HISTORY } from './constants/routes';
+
 import router from './router';
+
+setHook('addExpense', () => {
+  $router.push({ name: HISTORY });
+});
 
 export default {
   router,
